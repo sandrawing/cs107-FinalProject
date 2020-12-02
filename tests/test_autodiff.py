4b61e83bbcca30a6786825078de56192d4d6262d
xinzeng <50237270+sandrawing@users.noreply.github.com>
2020-12-03 00:37:31 +0800

    Update test_autodiff.py

--- tests/test_autodiff.py
+++ tests/test_autodiff.py
@@ -1,126 +1,162 @@
-import sys
+# import pytest
 import numpy as np
-sys.path.append('autodiff')
-
-import pytest
 from autodiff import AutoDiff
 
 
-def test_add_constant():
-    x = AutoDiff(5, 10)
+def test_add():
+    x = AutoDiff(5, 10, "x")
     f1 = x + 100
     assert f1.val == 105
-    assert f1.der == 10
+    assert f1.der["x"] == 10
 
+    x = AutoDiff(5, [10, 11], "x")
+    f1 = x + 100
+    assert f1.val == 105
+    assert np.array_equal(f1.der["x"], np.array([10, 11]))
 
-def test_add_object():
-    x = AutoDiff(5, 10)
-    y = AutoDiff(10, 1)
-    result = x + y
-    assert result.val == 15
-    assert result.der == 11
+    x = AutoDiff([8, 4], [10, 11], 'x')
+    y = AutoDiff([9, 12], [20, 33], 'y')
+    f1 = x + y
+    assert np.array_equal(f1.val, np.array([17, 16]))
+    assert np.array_equal(f1.der["x"], np.array([10, 11]))
+    assert np.array_equal(f1.der["y"], np.array([20, 33]))
 
 
 def test_radd():
-    x = AutoDiff(3, 10)
+    x = AutoDiff(3, 10, "x")
     f1 = 100 + x
     assert f1.val == 103
-    assert f1.der == 10
+    assert f1.der["x"] == 10
 
 
-def test_mul_constant():
-    x = AutoDiff(5, 30)
+def test_mul():
+    x = AutoDiff(5, 30, "x")
     f1 = x * 5
     assert f1.val == 25
-    assert f1.der == 150
+    assert f1.der["x"] == 150
 
-
-def test_mul_object():
-    x = AutoDiff(5, 15)
-    y = AutoDiff(2, 3)
+    x = AutoDiff(5, 15, "x")
+    y = AutoDiff(2, 3, "y")
     result = x * y
     assert result.val == 10
-    assert result.der == 45
+    assert result.der["x"] == 30
+    assert result.der["y"] == 15
+
+    x = AutoDiff([8, 4], name='x')
+    y = AutoDiff([9, 12], name='y')
+    f1 = x * y
+    assert np.array_equal(f1.val, np.array([72, 48]))
+    assert np.array_equal(f1.der["x"], np.array([9, 12]))
+    assert np.array_equal(f1.der["y"], np.array([8, 4]))
 
 
 def test_rmul():
-    x = AutoDiff(5, 20)
-    f1 = 5 * x
-    assert f1.val == 25
-    assert f1.der == 100
+    x = AutoDiff([8, 4], name='x')
+    y = AutoDiff([9, 12], name='y')
+    f1 = y * x
+    assert np.array_equal(f1.val, np.array([72, 48]))
+    assert np.array_equal(f1.der["x"], np.array([9, 12]))
+    assert np.array_equal(f1.der["y"], np.array([8, 4]))
 
 
-def test_sub_constant():
-    x = AutoDiff(5, 1)
+def test_sub():
+    x = AutoDiff(5, 10, "x")
     f1 = x - 100
     assert f1.val == -95
-    assert f1.der == 1
+    assert f1.der["x"] == 10
 
+    x = AutoDiff(5, [10, 11], "x")
+    f1 = x - 100
+    assert f1.val == -95
+    assert np.array_equal(f1.der["x"], np.array([10, 11]))
 
-def test_sub_object():
-    x = AutoDiff(5, 1)
-    y = AutoDiff(2, 3)
-    result = x - y
-    assert result.val == 3
-    assert result.der == -2
+    x = AutoDiff([8, 4], [10, 11], 'x')
+    y = AutoDiff([9, 12], [20, 33], 'y')
+    f1 = x - y
+    assert np.array_equal(f1.val, np.array([-1, -8]))
+    assert np.array_equal(f1.der["x"], np.array([10, 11]))
+    assert np.array_equal(f1.der["y"], np.array([-20, -33]))
 
 
 def test_rsub():
-    x = AutoDiff(5, 1)
+    x = AutoDiff(5, 1, "x")
     f1 = 100 - x
-    result = AutoDiff(95, -1)
-    assert f1 == result
+    assert f1.val == 95
+    assert f1.der["x"] == -1
 
 
-def test_pow_constant():
-    x = AutoDiff(2, 5)
+def test_pow():
+    x = AutoDiff(2, 5, "x")
     f1 = x ** 2
     assert f1.val == 4.0
-    assert f1.der == 20.0
+    assert f1.der["x"] == 20.0
 
-
-def test_pow_object():
-    x = AutoDiff(2, 1)
+    x = AutoDiff(2, 1, "x")
     f2 = x ** x
     assert f2.val == 4
-    assert f2.der == 4.0 * (np.log(x.val) + 1)
+    assert f2.der["x"] == 4.0 * (np.log(x.val) + 1)
+
+    x = AutoDiff(3, name='x')
+    y = AutoDiff(4, name='y')
+    f3 = (x ** 2) * (y ** 3)
+    assert np.array_equal(f3.val, np.array([576]))
+    assert np.array_equal(f3.der['x'], np.array([384]))
+    assert np.array_equal(f3.der['y'], np.array([432]))
+
+    x = AutoDiff([8, 4], name='x')
+    y = AutoDiff([9, 12], name='y')
+    f4 = (x ** 2) * (y ** 3)
+    assert np.array_equal(f4.val, np.array([46656, 27648]))
+    assert np.array_equal(f4.der['x'], np.array([11664, 13824]))
+    assert np.array_equal(f4.der['y'], np.array([15552, 6912]))
 
 
 def test_rpow():
-    x = AutoDiff(2, 1)
+    x = AutoDiff(2, name="x")
     f1 = 2 ** x
     assert f1.val == 4.0
-    assert f1.der == 4.0 * np.log(2)
+    assert f1.der["x"] == 4.0 * np.log(2)
 
 
-def test_truediv_constant():
-    x = AutoDiff(4, 3)
+def test_truediv():
+    x = AutoDiff(4, 3, "x")
     f1 = x / 2.0
     assert f1.val == 2.0
-    assert f1.der == 1.5
+    assert f1.der["x"] == 1.5
 
+    x = AutoDiff(2, name="x")
+    f2 = x / x
+    assert f2.val == [1]
+    assert f2.der['x'] == [0]
 
-def test_truediv_object():
-    x = AutoDiff(16, 12)
-    y = AutoDiff(8, 12)
-    result = x / y
-    assert result.val == 2.0
-    assert result.der == -1.5
+    x = AutoDiff([16, 0], name="x")
+    y = AutoDiff([8, -1], name="y")
+    f3 = x / y
+    assert np.array_equal(f3.val, np.array([2, 0]))
+    assert np.array_equal(f3.der['x'], np.array([0.125, -1.0]))
+    assert np.array_equal(f3.der['y'], np.array([-0.25, -0.0]))
+
+    # x = AutoDiff(16, 12, "x")
+    # y = AutoDiff(8, 12, "y")
+    # result = x / y
+    # print(result.der)
+    # assert result.val == 2.0
+    # assert result.der == -1.5
 
 
 def test_rtruediv():
     x_val = 2.0
-    x = AutoDiff(x_val)
+    x = AutoDiff(x_val, name="x")
     f1 = 2.0 / x
     assert f1.val == 2.0 / x_val
-    assert f1.der == (-2.0 / (x_val ** 2))
+    assert f1.der["x"] == -0.5
 
 
 def test_neg():
-    x = AutoDiff(2.0, 10.0)
+    x = AutoDiff(2.0, 10.0, "x")
     f1 = -x
     assert f1.val == -2.0
-    assert f1.der == -10.0
+    assert f1.der["x"] == -10.0
 
 
 def test_equal():
@@ -136,107 +172,107 @@ def test_unequal():
 
 
 def test_sin():
-    x = AutoDiff(2.0, 1.0)
+    x = AutoDiff(2, name="x")
+    x1 = AutoDiff.sin(x)
+    assert x1.val == [np.sin(2)]
+    assert x1.der['x'] == [np.cos(2)]
+
+    x = AutoDiff(2.0, 1.0, "x")
     f1 = AutoDiff.sin(3 * x + 2)
     assert f1.val == np.sin(8.0)
-    assert f1.der == 3 * np.cos(8.0)
+    assert f1.der["x"] == 3 * np.cos(8.0)
 
 
 def test_sinh():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.sinh(f1)
     assert f2.val == np.sinh(17)
-    assert f2.der == 3 * np.cosh(17)
+    assert f2.der["x"] == 3 * np.cosh(17)
 
 
 def test_cos():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.cos(f1)
     assert f2.val == np.cos(17)
-    assert f2.der == -3 * np.sin(17)
+    assert f2.der["x"] == -3 * np.sin(17)
 
 
 def test_cosh():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.cosh(f1)
     assert f2.val == np.cosh(17)
-    assert f2.der == 3 * np.sinh(17)
+    assert f2.der["x"] == 3 * np.sinh(17)
 
 
 def test_tan():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.tan(f1)
     assert f2.val == np.tan(17)
-    assert f2.der == 3 / ((np.cos(17)) ** 2)
+    assert f2.der["x"] == 3 / ((np.cos(17)) ** 2)
 
 
 def test_tanh():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.tanh(f1)
     assert f2.val == np.tanh(17)
-    assert f2.der == 3 / ((np.cosh(17)) ** 2)
+    assert f2.der["x"] == 3 / ((np.cosh(17)) ** 2)
 
 
 def test_sqrt():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.sqrt(f1)
     assert f2.val == (17) ** 0.5
-    assert f2.der == 3 / (2 * (17 ** 0.5))
+    assert f2.der["x"] == 3 / (2 * (17 ** 0.5))
 
 
 def test_ln():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.ln(f1)
     assert f2.val == np.log(17)
-    assert f2.der == (1 / 17) * 3
+    assert f2.der["x"] == (1 / 17) * 3
 
 
 def test_log():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.log(f1, 4)
     assert f2.val == np.log(17) / np.log(4)
-    assert f2.der == 3 / (np.log(4) * (17))
+    assert f2.der["x"] == 3 / (np.log(4) * (17))
 
 
 def test_exp():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.exp(f1)
     assert f2.val == np.exp(17)
-    assert f2.der == 3 * np.exp(17)
+    assert f2.der["x"] == 3 * np.exp(17)
 
 
 def test_exp_base():
-    x = AutoDiff(5.0, 1.0)
+    x = AutoDiff(5.0, 1.0, "x")
     f1 = 3 * x + 2
     f2 = AutoDiff.exp_base(f1, 4)
     assert f2.val == 4 ** 17
-    assert f2.der == (4 ** 17) * 3 * np.log(4)
+    assert f2.der["x"] == (4 ** 17) * 3 * np.log(4)
 
 
 if __name__ == '__main__':
-    test_add_constant()
-    test_add_object()
+    test_add()
     test_radd()
-    test_mul_constant()
-    test_mul_object()
+    test_mul()
     test_rmul()
-    test_sub_constant()
-    test_sub_object()
+    test_sub()
     test_rsub()
-    test_pow_constant()
-    test_pow_object()
+    test_pow()
     test_rpow()
-    test_truediv_constant()
-    test_truediv_object()
+    test_truediv()
     test_rtruediv()
     test_neg()
     test_sin()
@@ -250,3 +286,5 @@ if __name__ == '__main__':
     test_log()
     test_exp()
     test_exp_base()
+    # x = AutoDiff(2.0, 1.0)
+    # f1 = AutoDiff.sin((AutoDiff.cos(x) ** 2.0 + x ** 2.0) ** 0.5)
