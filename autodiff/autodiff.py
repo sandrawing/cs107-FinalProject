812218a76d08b885607e42c59d780f4165a84364
xinzeng <50237270+sandrawing@users.noreply.github.com>
2020-12-06 02:38:59 +0800

    add arc funcs

--- autodiff/autodiff.py
+++ autodiff/autodiff.py
@@ -10,8 +10,10 @@ class AutoDiff():
 
     def __init__(self, val, der=1, name="not_specified"):
         """
-        Initializes AutoDiff object with a value that was passed in and
-        sets the default value of derivative to 1
+        constructor for AutoDiff class
+        Initializes AutoDiff object with a value, derivative and name that was passed in
+        and converts the type of value to numpy array for handling multiple values
+        converts the type of derivatives to a dictionary for handling multiple variables
         """
         # Handle several input types of val, including float, int, list and np.ndarray
         if isinstance(val, (float, int)):
@@ -35,7 +37,7 @@ class AutoDiff():
 
     def get_variables(self):
         """
-        Get all variables in current AutoDiff object
+        returns the variable names
         """
         return set(self.der.keys())
 
@@ -170,6 +172,14 @@ class AutoDiff():
                 curr_val = np.array([other ** float(v) for v in self.val])
                 temp_der[variable] = np.log(other) * curr_val * self.der[variable]
             return AutoDiff(temp_val, temp_der, self.name)
+        elif isinstance(other, AutoDiff):
+            var_union = self.get_variables().union(other.get_variables())
+            temp_val = np.array([other.val ** float(v) for v in self.val])
+            for variable in var_union:
+                curr_val = np.array([other.val ** (float(v) - 1) for v in self.val])
+                temp_der[variable] = curr_val * (other.val * self.der.get(variable, 0) * np.log(other.val) +
+                                                 self.val * other.der.get(variable, 0))
+            return AutoDiff(temp_val, temp_der, self.name)
         else:
             raise TypeError("Invalid input type!")
 
@@ -204,9 +214,14 @@ class AutoDiff():
     def __eq__(self, other):
         """
         Overloads the equal comparision operator (==)
-        Inputs: AutoDiff Instance
-        Returns: True if self and other AutoDiff instance have the
-        same value and derivative; False if not
+        Inputs: Scalar or AutoDiff Instance
+        Returns:
+        If the input is scalar:
+        True if the length of val of self AutoDiff instance is 1 and
+        the value of element in self.val is same as other; False if not
+        If the input is AutoDiff Instance:
+        True if self and other AutoDiff instance have the
+        same values and same length of values; False if not
         """
         if isinstance(other, (int, float)):
             return np.array_equal(self.val, np.array([float(other)]))
@@ -216,9 +231,14 @@ class AutoDiff():
     def __ne__(self, other):
         """
         Overloads the not equal comparision operator (!=)
-        Inputs: AutoDiff Instance
-        Returns: False if self and other AutoDiff instance have the
-        same value and derivative; True if not
+        Inputs: Scalar or AutoDiff Instance
+        Returns:
+        If the input is scalar:
+        True if the length of val of self AutoDiff instance is not 1 or
+        the value of element in self.val is different from other; False if not
+        If the input is AutoDiff Instance:
+        True if self and other AutoDiff instance have different
+        values or different length of values; False if not
         """
         if isinstance(other, (int, float)):
             return not np.array_equal(self.val, np.array([float(other)]))
@@ -226,6 +246,12 @@ class AutoDiff():
             return not np.array_equal(self.val, other.val)
 
     def __lt__(self, other):
+        """
+        Overloads the less than comparision operator (<)
+        Inputs: Scalar or AutoDiff Instance
+        Returns:
+        Return the truth value of values (x1 < x2) element-wise
+        """
         if isinstance(other, (int, float)):
             if len(self.val) != 1:
                 raise TypeError("Please compare the variables with same number of values!")
@@ -236,6 +262,12 @@ class AutoDiff():
             return np.less(self.val, other.val)
 
     def __le__(self, other):
+        """
+        Overloads the less than or equal to comparision operator (<=)
+        Inputs: Scalar or AutoDiff Instance
+        Returns:
+        Return the truth value of values (x1 <= x2) element-wise
+        """
         if isinstance(other, (int, float)):
             if len(self.val) != 1:
                 raise TypeError("Please compare the variables with same number of values!")
@@ -246,6 +278,12 @@ class AutoDiff():
             return np.less_equal(self.val, other.val)
 
     def __gt__(self, other):
+        """
+        Overloads the greater than comparision operator (>)
+        Inputs: Scalar or AutoDiff Instance
+        Returns:
+        Return the truth value of values (x1 > x2) element-wise
+        """
         if isinstance(other, (int, float)):
             if len(self.val) != 1:
                 raise TypeError("Please compare the variables with same number of values!")
@@ -256,6 +294,12 @@ class AutoDiff():
             return np.greater(self.val, other.val)
 
     def __ge__(self, other):
+        """
+        Overloads the greater than or equal to comparision operator (>=)
+        Inputs: Scalar or AutoDiff Instance
+        Returns:
+        Return the truth value of values (x1 >= x2) element-wise
+        """
         if isinstance(other, (int, float)):
             if len(self.val) != 1:
                 raise TypeError("Please compare the variables with same number of values!")
@@ -270,7 +314,8 @@ class AutoDiff():
     def sin(self):
         """
         Inputs: None
-        Returns: A new AutoDiff object with the sine computation done on the value and derivative
+        Returns: A new AutoDiff object with the sine
+        computation done on the value and derivative
         """
         temp_der = {}
         new_val = np.sin(self.val)
@@ -336,6 +381,42 @@ class AutoDiff():
         temp_der = {}
         for variable in self.get_variables():
             temp_der[variable] = self.der[variable] * 1 / (np.cosh(self.val) ** 2)
+        return AutoDiff(new_val, temp_der, self.name)
+
+    def arcsin(self):
+        """
+        Inputs: None
+        Returns: A new AutoDiff object with the hyperbolic
+        arcsin computation done on the value and derivative
+        """
+        new_val = np.arcsin(self.val)
+        temp_der = {}
+        for variable in self.get_variables():
+            temp_der[variable] = self.der[variable] * 1 / np.sqrt(1 - self.val ** 2)
+        return AutoDiff(new_val, temp_der, self.name)
+
+    def arccos(self):
+        """
+        Inputs: None
+        Returns: A new AutoDiff object with the hyperbolic
+        arccos computation done on the value and derivative
+        """
+        new_val = np.arccos(self.val)
+        temp_der = {}
+        for variable in self.get_variables():
+            temp_der[variable] = -self.der[variable] * 1 / np.sqrt(1 - self.val ** 2)
+        return AutoDiff(new_val, temp_der, self.name)
+
+    def arctan(self):
+        """
+        Inputs: None
+        Returns: A new AutoDiff object with the hyperbolic
+        arctan computation done on the value and derivative
+        """
+        new_val = np.arctan(self.val)
+        temp_der = {}
+        for variable in self.get_variables():
+            temp_der[variable] = self.der[variable] * 1 / ((self.val ** 2) + 1)
         return AutoDiff(new_val, temp_der, self.name)
 
     def sqrt(self):
