c411ea2dc2a11ebc84069f46c96312176a3a6313
Sivananda Rajananda <>
2020-12-06 11:51:32 -0500

    Added files for distribution using PyPI

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autodiff-AsiaUnionCS107",
+    version="0.0.1",
+    author="AsiaUnionCS107",
+    description="An Automatic Differentiation Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AsiaUnionCS107/cs107-FinalProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
