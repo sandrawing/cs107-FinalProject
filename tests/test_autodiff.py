1e3128a0ee4d963c6b569a7dc84168b9781c436e
xinzeng <50237270+sandrawing@users.noreply.github.com>
2020-12-12 12:47:53 +0800

    Update test_autodiff.py

--- tests/test_autodiff.py
+++ tests/test_autodiff.py
@@ -1,10 +1,7 @@
-import sys
 import numpy as np
-sys.path.append('../autodiff')      # Enable test_autodiff.py to work locally
-sys.path.append('autodiff')
 
 import pytest
-from ad import AutoDiff
+from autodiff.ad import AutoDiff
 
 
 def test_init_fail():
